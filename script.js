e72fa0501a17da7bb03f40eba83a4f18c4818c1a
EcaCosca <enriquecoscarelli@gmail.com>
2023-12-04 10:51:11 +0100

    Checkpoint for a break

--- script.js
+++ script.js
@@ -3,6 +3,8 @@
 const from = document.querySelector("#name-form");
 const groceryFrom = document.querySelector("#grocery-form");
 const greeter = document.querySelector("h2");
+const ol = document.querySelector("ol");
+const clear = document.querySelector("#clear");
 
 const userName = localStorage.getItem("userName");
 
@@ -27,24 +29,51 @@ const handleSubmit = (e) => {
 from.addEventListener("submit", handleSubmit);
 
 // GROCERY EXAMPLE
+const addGroceryToList = (grocery) => {
+    const li = document.createElement("li");
+    li.innerHTML = grocery;
+    ol.appendChild(li);
+}
+
+
 // https://www.w3schools.com/js/tryit.asp?filename=tryjson_store
+// https://aws.amazon.com/documentdb/what-is-json/#:~:text=JavaScript%20Object%20Notation%2C%20more%20commonly,a%20wide%20variety%20of%20applications.
 const handleGrocerySubmit = (e) => {
     e.preventDefault();
+    let grocerysArr = []
     
+    if(localStorage.getItem("grocerys")){
+        grocerysArr = JSON.parse(localStorage.getItem("grocerys"))
+    }
+
     const grocery = e.target[0].value
 
-    let grocerysArr = ["apple", "orange", grocery]
+    grocerysArr.push(grocery)
     
     console.log(grocerysArr);
     console.log(typeof grocerysArr);
 
     localStorage.setItem("grocerys", JSON.stringify(grocerysArr));
 
-    const result = JSON.parse(localStorage.getItem("grocerys"))
 
-    console.log(result);
-    console.log(typeof result);
+    grocerysArr.forEach(element => addGroceryToList(element))
+
+
+    // const result = JSON.parse(localStorage.getItem("grocerys"))
+
+    // console.log(result);
+    // console.log(typeof result);
+
+    e.target[0].value = ""
 }
 
 groceryFrom.addEventListener("submit", handleGrocerySubmit);
+
+clear.addEventListener("click", () => {
+    // localStorage.clear();
+    localStorage.removeItem("userName");
+    localStorage.removeItem("grocerys");
+    ol.innerHTML = "";
+    greeter.innerText = `Welcome!`
+})
 
